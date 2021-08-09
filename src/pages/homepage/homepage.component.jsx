3a27183893a2a53db09f84085c53a92f098beeb9
Quentin Labadens <quentin.labadens@fabernovel.com>
2021-08-09 16:36:55 +0200

    create home page componentns

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,10 @@
+import './homepage.styles.scss';
+
+import Directory from "../../components/directory/directory.component";
+
+const HomePage = () => (
+    <div className='homepage'>
+        <Directory />
+    </div>
+)
+export default HomePage;
