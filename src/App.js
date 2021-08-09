d4f017ef64bbba1d855866c4a1d6fc38b80c99ae
Quentin Labadens <quentin.labadens@fabernovel.com>
2021-08-09 23:09:14 +0200

    collection items

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import './App.css';
+import { Switch, Route } from 'react-router-dom';
 import HomePage from "./pages/homepage/homepage.component";
+import { Component } from "react/cjs/react.production.min";
+import ShopPage from "./pages/shop/shop.component";
 
-function App() {
-  return (
-    <div>
-      <HomePage></HomePage>
-    </div>
-  );
+const HatsPage = () => (
+  <div>
+    <h1>Hats</h1>
+  </div>
+
+)
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Switch>
+          <Route exact path='/' component={HomePage} />
+          <Route path='/shop' component={ShopPage} />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
